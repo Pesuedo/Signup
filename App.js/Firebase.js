82e71e205a7e733ae492f1194c42ab01ce20e887
Pesuedo <princeasare19988@gmail.com>
2021-11-07 20:52:17 +0000

    Firebase

---
+++ App.js/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyACxn_22jcrNJYl2_UojT9Yc9XAQt6WBws",
+    authDomain: "login-page-c1c39.firebaseapp.com",
+    projectId: "login-page-c1c39",
+    storageBucket: "login-page-c1c39.appspot.com",
+    messagingSenderId: "336809862225",
+    appId: "1:336809862225:web:c0f2474a0034e07a9b8470"
+  };
